9a3bd26e07c6eea5fdd71090a8baebf602ed899e
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-02-13 14:23:28 +0100

    More debug base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/style/main.css'],
   app: {
-    baseURL: process.env.BASE_URL,
     head: {
       title: process.env.TITLE || "Quiz Java",
       link: [{
         rel: 'stylesheet',
         href: 'https://cdn.simplecss.org/simple.min.css'
       }]
-    }
-  },
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, '../docs')
     }
   },
 })
